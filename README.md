a1d4faa99caf51fa9603e319d525b43582714123
Egor <Egor@Henek>
2015-07-06 15:46:12 +0600

    pre alpha

---
+++ README.md
@@ -0,0 +1,46 @@
+Mixcloud Api Widget for Yii2
+========================================
+[![GitHub issues](https://img.shields.io/github/issues/EgorHenek/yii2-mixcloud.svg)](https://github.com/EgorHenek/yii2-mixcloud/issues)
+[![GitHub forks](https://img.shields.io/github/forks/EgorHenek/yii2-mixcloud.svg)](https://github.com/EgorHenek/yii2-mixcloud/network)
+[![GitHub stars](https://img.shields.io/github/stars/EgorHenek/yii2-mixcloud.svg)](https://github.com/EgorHenek/yii2-mixcloud/stargazers)
+[![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](https://raw.githubusercontent.com/EgorHenek/yii2-mixcloud/master/LICENSE)
+
+Installation
+------------
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```bash
+composer require EgorHenek/yii2-mixcloud:~1.0
+```
+or add
+
+```json
+"EgorHenek/yii2-mixcloud" : "~1.0"
+```
+
+to the require section of your application's `composer.json` file.
+
+Further Information
+-------------------
+Please, check the [MixCloud api site](https://www.mixcloud.com/developers/) documentation for further information about its configuration options.
+
+Contributing
+------------
+
+Please see [CONTRIBUTING](CONTRIBUTING.md) for details.
+
+Credits
+-------
+
+- [Egor Gluhih](https://github.com/EgorHenek)
+- [All Contributors](../../contributors)
+
+License
+-------
+
+The MIT License. Please see [License File](LICENSE.md) for more information.
+
+
+[edm.su](http://edm.su)
---
+++ src/Mixcloud.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * @copyright Copyright (c) 2015 Henek
+ * @link http://edm.su
+ * @license http://http://opensource.org/licenses/MIT MIT License
+ */
+namespace EgorHenek\mixcloud;
+
+use Yii;
+use yii\base\InvalidParamException;
+use yii\base\Component;
+use linslin\yii2\curl;
+
+/**
+ * Enhanced to work with api mixcloud.
+ * @see https://www.mixcloud.com/developers/
+ *
+ * @author Egor Henek <eagluhih@gmail.com>
+ * @link http://www.edm.su/
+ */
+class Mixcloud extends Component
+{
+    /**
+     * @var string 
+     */
+    public $clientToken;
+    
+    public function init ()
+    {
+        parent::init();
+        if (!$this->clientToken) {
+            throw new InvalidParamException;
+        }
+    }
+    
+    /**
+     * Upload mp3 file
+     * @param string $mp3 REQUIRED. The audio file to be uploaded. The file should not be larger than 524288000 bytes
+     * @param string $name REQUIRED (if a track). The track section song title
+     * @param array $params Other params (picture, description ...)
+     * @param array $tags Tags (maximum 5)
+     * @param array @playlis Playlist
+     */
+    public function upload ($mp3, $name, $params = [], $tags = [], $playlist = [])
+    {
+        $curl = new curl\Curl();
+        $request['mp3'] = $mp3;
+        $request['name'] = $name;
+        foreach ($params as $param => $value) {
+            $request[$param] = $value;
+        }
+        foreach (array_slice($tags, 0, 5) as $id => $tag) {
+            $request['tags-'.$id.'-tag'] = $tag;
+        }
+        foreach ($playlist as $id => $section) {
+            if (is_array($section)) {
+                foreach ($section as $key => $value) {
+                    $request['sections-'.$id.'-'.$key] = $value;
+                }
+            }else{
+                $request['sections-'.$id.'-chapter'] = $section;
+            }
+        }
+        $response = $curl->setOption(CURLOPT_POSTFIELDS, http_build_query($request))->post('https://api.mixcloud.com/upload/?access_token='.$this->clientToken);
+        return $response;
+    }
+    
+}
